f2136d139cbb1bff8d1662844973cbccdc84d77e
Anhphap2004 <phapmeo2k4@gmail.com>
2024-12-05 21:46:13 +0700

    Hiển thị sản phẩm theo danh mục

---
+++ ViewComponents/MenuFoodViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Souvenir.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+namespace Souvenir.ViewComponents
+{
+    public class MenuFoodViewComponent : ViewComponent
+    {
+        private readonly SouvenirShopDbContext _context;
+        public MenuFoodViewComponent(SouvenirShopDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int? CategoryID)
+        {
+            var menufood = CategoryID == null ? await _context.Products.ToListAsync() : await _context.Products.Where(h => h.CategoryId == CategoryID).ToListAsync();
+            return View(menufood);
+        }
+    }
+}
