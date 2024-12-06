3a202a72a15f0bca206a1caebbbd8188dd024262
Anhphap2004 <phapmeo2k4@gmail.com>
2024-12-06 14:01:47 +0700

    Ghép template Admin thành công

---
+++ Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Souvenir.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+namespace Souvenir.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
